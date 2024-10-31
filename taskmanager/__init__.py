c5b2b521359bb5c79e5579d7f2e1401f39887e18
Niall Carroll <niallpcarroll@gmail.com>
2024-10-31 19:12:09 +0000

    Create database

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env # noqa
+    import env  # noqa
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
